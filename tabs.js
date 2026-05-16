dac544cd8a6a5dc0adbb0922ec4b5d8d2a6b241b
ColeRollinPearson <colerollinpearson@gmail.com>
2026-05-16 01:54:40 -0500

    changed tab menu to a persistent tab bar, consolidated css styling, updated resume

--- tabs.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('tab-toggle');
-    const tabBar = document.getElementById('tab-bar');
-
-    toggleButton.addEventListener('click', function() {
-        tabBar.classList.toggle('hidden');
-    });
-});
